3dc2c4a181f66af28ed1df69cdcfdc09ec97c3dc
Özgür Rüzgar <94163712+ozgurruzgar@users.noreply.github.com>
2023-07-18 13:09:39 +0300

    Add project files.

---
+++ RabbirMQSubscriber/Program.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace RabbirMQSubscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://localhost:5672");
+
+            using var connection = factory.CreateConnection();
+            
+                var channel = connection.CreateModel();
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+            //channel.QueueDeclare("hello-queqe", true, false, false);
+
+            //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout); We can use this code in here.
+
+            //var randomQueqeName =channel.QueueDeclare().QueueName; //"log-database-save";
+
+            //channel.QueueDeclare(randomQueqeName, durable: true, false, false);
+
+            //channel.QueueBind(randomQueqeName, "logs-fanout", "", null); //When app down,about queqe will be remove. 
+
+            channel.BasicQos(0, 1, false); 
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                var queqeName = channel.QueueDeclare().QueueName;
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match","any");
+
+
+            channel.QueueBind(queqeName,"header-exchange",string.Empty,headers);
+
+            Console.WriteLine("Loglar dinleniyor...");
+
+                consumer.Received += (object sender, BasicDeliverEventArgs arg) =>
+                {
+                    var message = Encoding.UTF8.GetString(arg.Body.ToArray());
+
+                    Product product = JsonSerializer.Deserialize<Product>(message);
+
+                    Thread.Sleep(1500);
+
+                    Console.WriteLine($"Gelen Mesaj: { product.Id} - {product.Name} - {product.Price} - {product.Stock}");
+
+                    //File.AppendAllText("log-critical.txt", message + "\n"); // \n escape character semantically new line.
+
+                    channel.BasicAck(arg.DeliveryTag, false);
+                };
+            channel.BasicConsume(queqeName, false, consumer);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,80 @@
+﻿using RabbitMQ.Client;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQPublisher
+{
+    public enum LogNames
+    {
+        Critical=1,
+        Error=2,
+        Warning=3,
+        Info=4
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://localhost:5672");
+
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+                channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+                Dictionary<string,object> headers = new Dictionary<string, object>();
+
+                headers.Add("format","pdf");
+                headers.Add("shape2","a4");
+
+                var properties = channel.CreateBasicProperties();
+                properties.Headers= headers;
+                properties.Persistent = true; //Messages are will be persistent now.
+
+                var product = new Product { Id=1,Name="Kalem",Price=100,Stock=10};
+
+                var productJsonString = JsonSerializer.Serialize(product);
+
+                channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+
+                Console.WriteLine("Mesaj Gönderilmiştir.");
+
+                //Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+                //{
+                //   // var queqeName = $"direct-queqe-{x}";
+                //    //channel.QueueDeclare(queqeName, true, false, false);
+
+                //    //channel.QueueBind(queqeName, "logs-direct", routeKey,null);
+                //});
+
+                //Random rnd = new Random();
+                //Enumerable.Range(1, 50).ToList().ForEach(x =>
+                //{
+                //    LogNames log1 = (LogNames)rnd.Next(1, 5);
+                //    LogNames log2 = (LogNames)rnd.Next(1, 5);
+                //    LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+                //    var routeKey = $"{log1}.{log2}.{log3}";
+                //    //var routeKey = $"route-{log}";
+                //    string message = $"log-type {log1}-{log2}-{log3}";
+                //    var messageBody = Encoding.UTF8.GetBytes(message);
+
+                //    channel.BasicPublish("logs-topic",routeKey, null, messageBody);
+
+                //    Console.WriteLine($"Log Gönderilmiştir : {message}");
+                //});
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
